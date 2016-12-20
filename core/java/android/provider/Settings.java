a636633fb100c10879e97d8d618e9c4c589645df
thecrazyskull <anaskarbila@aospa.co>
2016-12-20 05:43:32 -0500

    Introduce color engine.
    Color engine is a system which allows changing the primary
    and accent colors of the device theme on-the-fly.
    
    Colors and design by @arzbhatia
    
    The following primary colors are supported:
    * White (android default)
    * Grey
    * Pixel
    * Dark
    
    The following accent colors are supported:
    * Blue
    * Red
    * Teal/Default
    * Orange
    * Green
    * Yellow
    * Purple
    * Pink
    * Cyan
    * Grey
    
    Ticket: NOUGAT-19
    
    Change-Id: I69347616e2e61e805647c2e1a3f3c4578f42847f
    Signed-off-by: @Thecrazyskull <anaskarbila@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6533,6 +6533,41 @@ public final class Settings {
                 "managed_profile_contact_remote_search";
 
         /**
+
+         * Whether to include options in power menu for rebooting into recovery or bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
+         * Boolean value whether to link ringtone and notification volume
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
+         * Whether dark theme is enabled or not
+         *
+         * @hide
+         */
+        public static final String THEME_PRIMARY_COLOR = "theme_primary_color";
+
+        /**
+         * Whether accent colors for themes are enabled or not
+         *
+         * @hide
+         */
+        public static final String THEME_ACCENT_COLOR = "theme_accent_color";
+
+        /**
+         * Whether accent colors or dark mode is supposed to show first
+         *
+         * @hide
+         */
+        public static final String THEME_SETTINGS_MODE = "theme_settings_mode";
+
+        /**
+
          * Whether or not the automatic storage manager is enabled and should run on the device.
          *
          * @hide
